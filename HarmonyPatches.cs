15d509bd3b605fd530a59a1655b731bd80a6c775
Szturmowiec <piotr.cwiklicki@gmail.com>
2023-01-10 22:05:42 +0100

    Add project files.

---
+++ HarmonyPatches.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace IllegalPsycasts
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+        private static readonly Type patchType = typeof(HarmonyPatches);
+        static HarmonyPatches()
+        {
+            Harmony harmony = new Harmony(id: "illegal_psycasts_harmony");
+            harmony.Patch(AccessTools.Method(typeof(Psycast), nameof(Psycast.Activate),
+                new Type[] { typeof(LocalTargetInfo), typeof(LocalTargetInfo) }),
+                postfix: new HarmonyMethod(patchType, nameof(notifyEmpireAboutIllegalPsycast)));
+
+            harmony.Patch(AccessTools.Method(typeof(Psycast), nameof(Psycast.Activate), new Type[] { typeof(GlobalTargetInfo) }),
+                postfix: new HarmonyMethod(patchType, nameof(notifyEmpireAboutIllegalPsycast)));
+
+            harmony.Patch(AccessTools.Method(typeof(ThingRequiringRoyalPermissionUtility),
+                nameof(ThingRequiringRoyalPermissionUtility.GetMinTitleToUse)),
+                postfix: new HarmonyMethod(patchType, nameof(getMinTitleForImplantPatched)));
+
+            harmony.Patch(AccessTools.Method(typeof(ThingRequiringRoyalPermissionUtility),
+                nameof(ThingRequiringRoyalPermissionUtility.IsViolatingRulesOfAnyFaction)),
+                postfix: new HarmonyMethod(patchType, nameof(IsViolatingRulesOfAnyFactionPatched)));
+
+            harmony.Patch(AccessTools.Method(typeof(Verb_CastPsycast), nameof(Verb_CastPsycast.OnGUI)),
+                prefix: new HarmonyMethod(patchType, nameof(displayDetectionChance)));
+        }
+
+        public static void notifyEmpireAboutIllegalPsycast(ref Psycast __instance)
+        {
+            Faction empire = Find.FactionManager.OfEmpire;
+            if (PsycastDetectionutils.isPsycastIllegal(__instance.pawn, empire, __instance.def.level))
+            {
+                float detectionChance = PsycastDetectionutils.getDetectionChance(__instance.def);
+                if (!Rand.Chance(detectionChance))
+                {
+                    return;
+                }
+                empire.Notify_RoyalThingUseViolation(__instance.def, __instance.pawn, __instance.def.defName, detectionChance, __instance.def.level);
+            }
+        }
+
+        public static void IsViolatingRulesOfAnyFactionPatched(ref bool __result, Pawn pawn, int implantLevel)
+        {
+            foreach (Faction faction in Find.FactionManager.AllFactions)
+            {
+                __result = PsycastDetectionutils.isPsycastIllegal(pawn, faction, implantLevel);
+                if (__result)
+                {
+                    return;
+                }
+            }
+        }
+
+        public static void getMinTitleForImplantPatched(ref RoyalTitleDef __result, int implantLevel)
+        {
+            List<RoyalTitleDef> availableTitles = Find.FactionManager.OfEmpire.def.RoyalTitlesAwardableInSeniorityOrderForReading;
+            __result = availableTitles.Find(title => title.maxPsylinkLevel == implantLevel);
+        }
+
+        public static void displayDetectionChance(Verb_CastPsycast __instance)
+        {
+            __instance.Psycast.def.detectionChanceOverride = PsycastDetectionutils.getDetectionChance(__instance.Psycast.def);
+        }
+    }
+}
---
+++ PsycastDetectionUtils.cs
@@ -0,0 +1,38 @@
+﻿using RimWorld;
+using System.Linq;
+using Verse;
+
+namespace IllegalPsycasts
+{
+    public static class PsycastDetectionutils
+    {
+        static PsycastDetectionutils()
+        {
+
+        }
+
+        public static bool isPsycastIllegal(Pawn pawn, Faction faction, int implantLevel)
+        {
+            if (!Find.FactionManager.OfEmpire.Equals(faction) || hasSilencer(pawn))
+            {
+                return false;
+            }
+            RoyalTitleDef title = pawn.royalty.GetCurrentTitle(faction);
+            return title == null || implantLevel > title.index;
+        }
+
+        public static float getDetectionChance(AbilityDef psycast)
+        {
+            if (psycast.EntropyGain == 0)
+            {
+                return (float) psycast.level / 10;
+            }
+            return psycast.EntropyGain / 100;
+        }
+
+        private static bool hasSilencer(Pawn pawn)
+        {
+            return pawn.health.hediffSet.hediffs.Any(implant => implant.def.defName.Equals("HediffPsychicSilencer"));
+        }
+    }
+}
